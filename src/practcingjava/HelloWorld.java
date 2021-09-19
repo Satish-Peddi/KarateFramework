28d6d3da3cc255a7b188051bbc6f655ff2b11f82
Satish-Peddi <44740483+Satish-Peddi@users.noreply.github.com>
2021-09-19 19:35:37 +0530

    added for loop in Java class

--- src/practcingjava/HelloWorld.java
+++ src/practcingjava/HelloWorld.java
@@ -4,7 +4,11 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a = 5;
 		System.out.println("Hello World");
+		for(int i=0;i<a;i++) {
+			System.out.println("Trying to add the project into the github");
+		}
 
 	}
 
