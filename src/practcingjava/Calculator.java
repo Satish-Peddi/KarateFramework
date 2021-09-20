6e127226e3696cdeff39a5b7db54690bdd54a744
Satish-Peddi <44740483+Satish-Peddi@users.noreply.github.com>
2021-09-20 23:48:24 +0530

    added new file named calculator

---
+++ src/practcingjava/Calculator.java
@@ -0,0 +1,15 @@
+package practcingjava;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is for Calculator working");
+		Scanner scan = new Scanner("text.xml");
+		scan.nextInt();
+	}
+
+}
