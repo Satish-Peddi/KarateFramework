868678e1247a5d099be2af675dfbbb1ace12de43
Satish-Peddi <44740483+Satish-Peddi@users.noreply.github.com>
2021-09-20 23:18:31 +0530

    Deleted Hello world printing

--- src/practcingjava/HelloWorld.java
+++ src/practcingjava/HelloWorld.java
@@ -8,7 +8,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a = 5;
-		System.out.println("Hello World");
 		System.out.println(sum(11, 55));
 		for (int i = 0; i < a; i++) {
 			System.out.println("Trying to add the project into the github");
